3c61de6e41041fcbb0f8aaf9f6840b5d17612013
Oleksii Nazarenko <alexeybread81@gmail.com>
2022-12-03 11:08:07 +0000

    add wasd to player input

--- core/interact_logic.py
+++ core/interact_logic.py
@@ -1,58 +1,94 @@
 import pygame
 import CONSTANTS
+from main import *
+from CONSTANTS import *
+from core.movement_logic import *
+
+pygame.init()
 
 
-def movement(VELOCITY, character_rectangle, keys_pressed):
-    if (keys_pressed[pygame.K_UP] or keys_pressed[pygame.K_w]) and character_rectangle.y >= 0:
-        character_rectangle.y -= VELOCITY
-    if (keys_pressed[pygame.K_DOWN] or keys_pressed[pygame.K_s]) and character_rectangle.y <= CONSTANTS.HEIGHT - CONSTANTS.SCALE_HEIGHT:
-        character_rectangle.y += VELOCITY
-    if (keys_pressed[pygame.K_RIGHT] or keys_pressed[pygame.K_d]) and character_rectangle.x <= CONSTANTS.WIDTH - CONSTANTS.SCALE_WIDTH:
-        character_rectangle.x += VELOCITY
-    if (keys_pressed[pygame.K_LEFT] or keys_pressed[pygame.K_a]) and character_rectangle.x >= 0:
-        character_rectangle.x -= VELOCITY
+text_font = pygame.font.SysFont(None, 30)
 
-    return character_rectangle.x, character_rectangle.y
+#Coordinates and sizes
+text_box_size = (10, 600, 500, 100)
+
+simulator_rect = pygame.Rect(200, 125, 30, 30)
 
 
-def turn(character, keys_pressed):
-    if True not in keys_pressed:
-        return CONSTANTS.CHARACTER_FRONT
-    if keys_pressed[pygame.K_RIGHT]:
-        CONSTANTS.CHARACTER_RIGHT += 1
-        if CONSTANTS.CHARACTER_RIGHT < CONSTANTS.PACE:
-            return CONSTANTS.CHARACTER_RIGHT_WALK
-        elif CONSTANTS.CHARACTER_RIGHT < 2*CONSTANTS.PACE:
-            return CONSTANTS.CHARACTER_RIGHT_STAY
+def drawObject(color):
+    pygame.draw.rect(WINDOW, color, simulator_rect)
+
+
+def drawOptions(opt1, opt2, opt3):
+
+    pygame.draw.rect(WINDOW, WHITE, opt1)
+    pygame.draw.rect(WINDOW, WHITE, opt2)
+    pygame.draw.rect(WINDOW, WHITE, opt3)
+
+
+def drawText(text):
+    # drawing the  text box
+    text_box = pygame.Rect(text_box_size)
+    pygame.draw.rect(WINDOW, WHITE, text_box)
+    pygame.draw.rect(WINDOW, BLACK, (10, 595, 500, 5))  # Top border
+    pygame.draw.rect(WINDOW, BLACK, (5, 600, 5, 100))  # Left border
+    pygame.draw.rect(WINDOW, BLACK, (510, 600, 5, 100))  # Right border
+    pygame.draw.rect(WINDOW, BLACK, (10, 700, 500, 5))  # Bottom border
+
+    # Displays text within the text box borders
+    bkg = None
+    y = text_box.top
+    lineSpacing = -2
+    while text:
+        i = 1
+        if y + 20 > text_box.bottom:
+            break
+        while text_font.size(text[:i])[0] < text_box.width and i < len(text):
+            i += 1
+        if i < len(text):
+            i = text.rfind(' ', 0, i) + 1
+        if bkg:
+            comment = text_font.render(text[:i], True, BLACK, bkg)
+            image.set_colorkey(bkg)
         else:
-            CONSTANTS.CHARACTER_RIGHT = 0
-            return CONSTANTS.CHARACTER_RIGHT_WALK
-    if keys_pressed[pygame.K_LEFT]:
-        CONSTANTS.CHARACTER_LEFT += 1
-        if CONSTANTS.CHARACTER_LEFT < CONSTANTS.PACE:
-            return CONSTANTS.CHARACTER_LEFT_WALK
-        elif CONSTANTS.CHARACTER_LEFT < 2*CONSTANTS.PACE:
-            return CONSTANTS.CHARACTER_LEFT_STAY
-        else:
-            CONSTANTS.CHARACTER_LEFT = 0
-            return CONSTANTS.CHARACTER_LEFT_WALK
-    if keys_pressed[pygame.K_UP]:
-        CONSTANTS.CHARACTER_UP += 1
-        if CONSTANTS.CHARACTER_UP < CONSTANTS.PACE:
-            return CONSTANTS.CHARACTER_BACK_LEFT
-        elif CONSTANTS.CHARACTER_UP < 2*CONSTANTS.PACE:
-            return CONSTANTS.CHARACTER_BACK_RIGHT
-        else:
-            CONSTANTS.CHARACTER_UP = 0
-            return CONSTANTS.CHARACTER_BACK_LEFT
-    if keys_pressed[pygame.K_DOWN]:
-        CONSTANTS.CHARACTER_DOWN += 1
-        if CONSTANTS.CHARACTER_DOWN < CONSTANTS.PACE:
-            return CONSTANTS.CHARACTER_FRONT_LEFT
-        elif CONSTANTS.CHARACTER_DOWN < 2*CONSTANTS.PACE:
-            return CONSTANTS.CHARACTER_FRONT_RIGHT
-        else:
-            CONSTANTS.CHARACTER_DOWN = 0
-            return CONSTANTS.CHARACTER_FRONT_LEFT
+            comment = text_font.render(text[:i], False, BLACK)
 
-    return character
+        WINDOW.blit(comment, (text_box.left, y))
+        y += 20 + lineSpacing
+
+        text = text[i:]
+
+
+def interact(character_rectangle, keys_pressed):
+    collideProblem = character_rectangle.colliderect(simulator_rect)
+    point = pygame.mouse.get_pos()
+    # Objects changes color once the character is in distance
+    if not collideProblem:
+        color = BLUE
+        text = "Let's have a look!"
+    else:
+        color = RED
+        text = "Hmm, something's off! It's seems that there is a part missing!"
+
+    if collideProblem and keys_pressed[pygame.K_RETURN]:
+        text = 'EXPLAINING THE PROBLEM (LALALA THIS AND THAT)'
+        opt1 = pygame.Rect(550, 600, 100, 100)
+        opt2 = pygame.Rect(700, 600, 100, 100)
+        opt3 = pygame.Rect(850, 600, 100, 100)
+        drawOptions(opt1, opt2, opt3)
+
+        collideAnswer1 = opt1.collidepoint(point)
+        if collideAnswer1:
+            text = 'HMM, Possibly (Explain this answer) OPT 1'
+
+        collideAnswer2 = opt2.collidepoint(point)
+        if collideAnswer2:
+            text = 'HMM, Possibly (Explain this answer) OPT 2'
+
+        collideAnswer3 = opt3.collidepoint(point)
+        if collideAnswer3:
+            text = 'HMM, Possibly (Explain this answer) OPT 3'
+
+    else:
+        object_coordinates = (200, 125, 30, 30)
+    return text, color
